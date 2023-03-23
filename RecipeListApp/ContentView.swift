18e895ea6487592d32e48759127ebdff1f937d38
Ayush-Prasad4 <89988134+Ayush-Prasad4@users.noreply.github.com>
2023-03-23 15:39:10 +0530

    Initial Commit

---
+++ RecipeListApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecipeListApp
+//
+//  Created by Ayush Prasad on 23/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Ayush Prasad on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
